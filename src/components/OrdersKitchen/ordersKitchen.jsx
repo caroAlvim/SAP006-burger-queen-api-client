fabaa070f1d8edf3c88e04b65cd83e1d36dc36e3
Carolina Alvim <caro.alvim@gmail.com>
2021-09-20 15:13:21 -0300

    feat:inicio dos pedidos

---
+++ src/components/OrdersKitchen/ordersKitchen.jsx
@@ -0,0 +1,38 @@
+import React from 'react';
+
+// eslint-disable-next-line no-empty-pattern
+function OrdersKitchen({
+//   name,
+//   flavor,
+//   complement,
+//   qtd,
+}) {
+  return (
+    <>
+      <div>
+        <h2> Pedido </h2>
+      </div>
+    </>
+  );
+}
+
+export default OrdersKitchen;
+
+// {
+//     "id": 2109,
+//     "client_name": "ana",
+//     "user_id": 2117,
+//     "table": 1,
+//     "status": "pending",
+//     "processedAt": null,
+//     "createdAt": "2021-09-17T05:01:09.132Z",
+//     "updatedAt": "2021-09-17T05:01:09.132Z",
+//     "Products": [
+//       {
+//         "id": 29,
+//         "name": "Café americano",
+//         "flavor": null,
+//         "complement": null,
+//         "qtd": 1
+//       }]
+// }
