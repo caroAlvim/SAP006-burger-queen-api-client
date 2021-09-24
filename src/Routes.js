680f112a0faab2091a6fede64c59aac4fbbaa88f
jjullimayanne <julli.souza@ufob.edu.br>
2021-09-24 02:23:49 -0300

    pagina menu refatorada, faltando ainda ajustes de css

--- src/Routes.js
+++ src/Routes.js
@@ -12,8 +12,6 @@ import Page404 from './pages/Page404/index';
 import Pedidos from './pages/Pedidos/breakfast/index';
 import Kitchen from './pages/Kitchen/kitchen';
 import GlobalStyle from './components/GlobalStyle';
-import AllDay from './pages/Pedidos/allDay/index';
-import Burguer from './pages/Pedidos/allDay/burguer';
 
 import PrivateRoute from './service/PrivateRoute';
 
@@ -65,14 +63,6 @@ function Routes() {
         <PrivateRoute exact path="/kitchen">
           <Kitchen />
         </PrivateRoute>
-
-        <Route exact path="/allday/">
-          <AllDay />
-        </Route>
-
-        <Route exact path="/simpleBurguers/">
-          <Burguer />
-        </Route>
 
         <Route>
           <Page404 />
--- src/pages/Pedidos/breakfast/index.js
+++ src/pages/Pedidos/breakfast/index.js
@@ -1,33 +1,35 @@
 /*eslint-disable */
 import './index.css';
 import { useState, useEffect } from 'react';
-import { useParams } from 'react-router-dom';
+import { useParams, useHistory } from 'react-router-dom';
 import Header from '../../../components/Header/header';
 import Item from '../../../components/Item/itens';
 import Button from '../../../components/Button/button';
-import { useHistory } from 'react-router-dom';
 
+import Panel from '../../../components/Menu/menu';
 
 function Pedidos() {
   useEffect(() => {
     getAllProducts();
   }, []);
+
   const { mesa } = useParams();
+
   const history = useHistory();
 
   function somaFinal(array) {
     return array.reduce((total, item) => total + (item.qtd * item.price), 0);
   }
 
-  const allDay = () => {
-    history.push('/allday');
-  };
+  const [menus, setMenus] = useState(true);
+  const [menuAlmoco, setMenuAlmoco] = useState([]);
+  const [menuCafe, setMenuCafe] = useState([]);
 
-  const [menu, setMenu] = useState('');
   const [resumopedido, setResumoPedido] = useState([]);
   const [fazerPedido, setFazerPedido] = useState({ client: '', table: mesa, products: [] });
   const [erroMessage, setErroMessage] = useState('');
   const [showError, setShowError] = useState(false);
+
   const token = localStorage.getItem('token');
   const getAllProducts = () => {
     fetch('https://lab-api-bq.herokuapp.com/products', {
@@ -41,63 +43,132 @@ function Pedidos() {
       .then((response) => response.json())
       .then((json) => {
         const breakfast = json.filter((item) => item.type === 'breakfast');
-        setMenu(breakfast);
+        setMenuCafe(breakfast);
+        const allDayMenu = json.filter((item) => item.type === 'all-day');
+        setMenuAlmoco(allDayMenu);
       });
   };
 
   return (
     <main className="all-container">
       <Header />
-     
-      <div className="menu-um">
-        <section>
-        <Button
-      buttonClass="menu-button"
-        buttonOnClick={(e) => allDay(e)}
-        >Café da Manhã
-        </Button>
-        <Button
-      buttonClass="menu-button"
-        buttonOnClick={(e) => allDay(e)}
-        >All Day
-        </Button>
+      <div className="">
 
-        </section>
-     
-        <div className="breakfast-menu">
-          {menu && menu.map((item, index) => (
-            <Item
-              divClassName="container-food"
-              divKey={Math.random()}
-              itemName={item.name}
-              divId={item.id}
-              ImgSrc={item.image}
-              itemPrice={item.price}
-              qnt={item.qnt}
-              itemNameKey={item.id}
-              divOnClick={() => {
-                if (!resumopedido.some((item) => item.name === menu[index].name)) {
-                  setResumoPedido([...resumopedido, {
-                    id: menu[index].id,
-                    name: menu[index].name,
-                    price: menu[index].price,
-                    qtd: 1,
-                  }]);
-                } else {
-                  resumopedido.map((item, i) => {
-                    if (item.name === menu[index].name) {
-                      resumopedido[i].qnt++
+        {menus ? (
+          <Panel>
+            <section>
+              <Button
+                buttonClass="menu-button"
+                buttonOnClick={() => {
+                  setMenus(true);
+                }}
+              >Café da Manhã
+              </Button>
+              <Button
+
+                buttonClass="menu-button"
+                buttonOnClick={() => {
+                  setMenus(false);
+                }}
+              >All Day
+              </Button>
+
+            </section>
+            <div className="breakfast-menu">
+              {menuCafe && menuCafe.map((item, index) => (
+                <Item
+                  divClassName="container-food"
+                  divKey={Math.random()}
+                  itemName={item.name}
+                  divId={item.id}
+                  ImgSrc={item.image}
+                  itemPrice={item.price}
+                  qnt={item.qnt}
+                  itemNameKey={item.id}
+                  divOnClick={() => {
+                    if (!resumopedido.some((item) => item.name === menuCafe[index].name)) {
+                      setResumoPedido([...resumopedido, {
+                        id: menuCafe[index].id,
+                        name: menuCafe[index].name,
+                        price: menu[index].price,
+                        qtd: 1,
+                      }]);
+                    } else {
+                      resumopedido.map((item, i) => {
+                        if (item.name === menu[index].name) {
+                          resumopedido[i].qnt++
                     + setResumoPedido([...resumopedido]);
+                        }
+                      });
                     }
-                  });
-                }
-              }}
+                  }}
+                />
 
-            />
+              ))}
 
-          ))}
+            </div>
 
-        </div>
+          </Panel>
+
+        ) : (
+          <Panel>
+             <section>
+              <Button
+                buttonClass="menu-button"
+                buttonOnClick={() => {
+                  setMenus(true);
+                }}
+              >Café da Manhã
+              </Button>
+              <Button
+
+                buttonClass="menu-button"
+                buttonOnClick={() => {
+                  setMenus(false);
+                }}
+              >All Day
+              </Button>
+
+            </section>
+          <div className="breakfast-menu">
+            {menuAlmoco && menuAlmoco.map((item, index) => (
+              <Item
+                divClassName="container-food"
+                divKey={Math.random()}
+                itemName={item.name}
+                divId={item.id}
+                ImgSrc={item.image}
+                itemPrice={item.price}
+                qnt={item.qnt}
+                itemNameKey={item.id}
+                divOnClick={() => {
+                  if (!resumopedido.some((item) => item.name === menuAlmoco[index].name)) {
+                    setResumoPedido([...resumopedido, {
+                      id: menuAlmoco[index].id,
+                      name: menuAlmoco[index].name,
+                      price: menuAlmoco[index].price,
+                      qtd: 1,
+                    }]);
+                  } else {
+                    resumopedido.map((item, i) => {
+                      if (item.name === menu[index].name) {
+                        resumopedido[i].qnt++
+                + setResumoPedido([...resumopedido]);
+                      }
+                    });
+                  }
+                }}
+              />
+
+            ))}
+
+          </div>
+          </Panel>
+
+        )}
+        
+
+      
       </div>
       <div className="finish-menu">
 
