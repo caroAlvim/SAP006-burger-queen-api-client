50b4bf89fb757ece669b82697baa6a998c9b5243
Carolina Alvim <caro.alvim@gmail.com>
2021-10-02 21:35:17 -0300

    adicionando ocean flower no header

--- src/components/Header/header.jsx
+++ src/components/Header/header.jsx
@@ -1,6 +1,9 @@
 import React from 'react';
 import { useHistory } from 'react-router-dom';
 import Logo from '../../img/logo-img.png';
+import Ocean from '../../img/3.png';
+import OceanFlower from '../../img/2.png';
+import OceanFlo from '../../img/1.png';
 import Button from '../Button/button';
 import './header.css';
 
@@ -17,9 +20,11 @@ function Header() {
   return (
     <>
       <header>
-
         <div className="logo-login">
           <img className="logo-img" src={Logo} alt="Logo The Krusty Krab" />
+          <img className="ocean-img" src={Ocean} alt="ocean-flower" />
+          <img className="ocean-img-flo" src={OceanFlower} alt="ocean-flower" />
+          <img className="ocean-img-flow" src={OceanFlo} alt="ocean-flower" />
         </div>
         <div className="bubble-1 bubble-container anim bubble-animation-x">
           <div className="bubble bubble-animation-y" />
